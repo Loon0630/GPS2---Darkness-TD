c4e62268c23649a6061f0f3ed590f56903c46b72
Karseng <karseng219@gmail.com>
2019-09-30 15:28:26 +0800

    Use to Build

---
+++ Tower Defence 9.29/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("More than one BuildManager");
+        }
+        instance = this;
+
+    }
+
+    public GameObject standardTurrentPrefab;
+
+    private void Start()
+    {
+        turrentToBuild = standardTurrentPrefab;
+    }
+
+    private GameObject turrentToBuild;
+
+    public GameObject GetTurrentToBuild ()
+    {
+        return turrentToBuild;
+    }
+}
