69e17a2d7440d1b1a3cb8620050e434b9d911624
SeeAikEng <0124714@kdu-online.com>
2019-10-10 22:01:01 +0800

    RocketBullet

--- Tower Defence 9.29/Assets/Scripts/Bullet.cs
+++ Tower Defence 9.29/Assets/Scripts/Bullet.cs
@@ -5,6 +5,8 @@ public class Bullet : MonoBehaviour
     public int damage = 50;
     private Transform target;
     [SerializeField] private float speed;
+    [SerializeField] private float expRadius = 0f;
+    [SerializeField] private GameObject impactEffect;
 
 
     public void Chase(Transform _target)
@@ -51,20 +53,71 @@ public class Bullet : MonoBehaviour
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
 
+        transform.LookAt(target);
+
     }
 
     void HitTarget()
     {
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 2f);
+
+        if(expRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
+        Destroy(target.gameObject);
         Destroy(gameObject);
     }
 
     void HitTargetRight()
     {
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 2f);
+
+        if (expRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
+        Destroy(target.gameObject);
         Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, expRadius);
+        foreach(Collider collider in colliders)
+        {
+            if(collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
     }
 
     void Die()
     {
         GameObject.Destroy(this.gameObject);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, expRadius);
     }
 }
